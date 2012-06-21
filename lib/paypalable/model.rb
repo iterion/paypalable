4954cff7277b4163b0b4b3aa1e8996d452a596ef
Adam Sunderland <iterion@gmail.com>
2012-06-21 00:07:44 -0500

    Some hacks - I'll fix them someday

--- lib/paypalable/model.rb
+++ lib/paypalable/model.rb
@@ -8,26 +8,28 @@ module ActiveRecord
     module ClassMethods
       def paypalable(options = {})
         attr_accessor :paypalable_options, :payment_response
-        @paypalable_options = options.reverse_merge!({
+        @paypalable_options = {
           payment_method: :payment_data
-        })
+        }
         include ActiveRecord::Paypalable::InstanceMethods
       end
     end
 
     module InstanceMethods
       def payment_method
+        @paypalable_options ||= {:payment_method => :payment_data}
         @paypalable_options[:payment_method]
       end
 
       def pay(host)
-        payment_response = ::Paypalable::Request.pay paypal_payment_data host
+        @payment_response = ::Paypalable::Request.pay paypal_payment_data host
 
-        unless payment_response.success?
+
+        unless @payment_response.success?
           self.errors.add(:base, @payment_response.errors.first['message'])
         end
-        puts payment_response
-        payment_response.success?
+        
+        @payment_response.success?
       end
       
       def paypal_payment_data(host)
