3facc8155c6faba081bec65afff1f53b05b7db33
Adam Sunderland <iterion@gmail.com>
2012-06-20 20:15:29 -0500

    Set pay_response as an object of the model.

--- lib/paypalable/model.rb
+++ lib/paypalable/model.rb
@@ -6,19 +6,32 @@ module ActiveRecord
     end
 
     module ClassMethods
-      def paypalable(payment_method = :payment_data)
+      def paypalable(options = {})
+        @paypalable_options = options.reverse_merge! {
+          payment_method: :payment_data
+        }
         @payment_method = payment_method.to_sym
         include ActiveRecord::Paypalable::InstanceMethods
       end
     end
 
     module InstanceMethods
+      def payment_method
+        @paypalable_options[:payment_method]
+      end
+
       def payment_response
         @payment_response
       end
 
       def pay(host)
         @payment_response = ::Paypalable::Request.pay paypal_payment_data host
+
+        unless @payment_response.success?
+          self.errors = self.errors + @payment_response.errors
+        end
+
+        @payment_response
       end
       
       def paypal_payment_data(host)
@@ -27,7 +40,7 @@ module ActiveRecord
           "currencyCode"=>"USD",
           "actionType"=>"PAY"
         }
-        self.send(:payment_data, data, host)
+        self.send(payment_method, data, host)
       end
       
     end #InstanceMethods
